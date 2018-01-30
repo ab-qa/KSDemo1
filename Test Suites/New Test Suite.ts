703937db7556015f2ef92427f869c1b7071f82d1
abhishek.a.a.kumar <abhishek.a.a.kumar@cpx-nzfjbw72rc5.dir.svc.accenture.com>
2018-01-30 14:49:21 +0800

    First commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7868dd9f-45cf-48db-bf93-2f61ea76eb8d</testSuiteGuid>
+</TestSuiteEntity>
